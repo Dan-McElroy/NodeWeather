50658afe3130ee9ed52f9d67d0218ac98056e573
Dan McElroy <daniel@mcelroys.net>
2021-01-26 22:03:19 +0100

    End of 33. An HTTP Request Challenge.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,26 @@
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=43102757838af7ddfab80fb1c51eaacc&query=37.8267,-122.423'
-
 request({ 
-    url: url,
+    url: 'http://api.weatherstack.com/currents',
+    qs: {
+        access_key: '43102757838af7ddfab80fb1c51eaacc',
+        query: '37.8267,-122.423'
+    },
     json: true 
 }, (error, response) => {
     const current = response.body.current
     console.log('%s: it is currently %f degrees. It feels like %f degrees out.', 
         current.weather_description[0], current.temperature, current.feelslike)
+})
+
+request({
+    url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json',
+    qs: {
+        access_token: 'pk.eyJ1IjoiZGFubWNlbHJveSIsImEiOiJja2tlZ3VxYWMwMG1iMnZtcDB1Nzl2ZGppIn0.yTmkyzgh1CUMVQdGCf4MBg',
+        limit: 1
+    },
+    json: true
+}, (error, response) => {
+    const coordinates = response.body.features[0].center
+    console.log('Latitude: %f, Longitude: %f', coordinates[1], coordinates[0])
 })
