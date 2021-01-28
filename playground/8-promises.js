0a61a357ed7f7d9eb9156f5d00387949f79e3630
Dan McElroy <daniel@mcelroys.net>
2021-01-28 16:39:28 +0100

    End of 93. Promise Chaining.

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,25 @@
-const doWorkPromise = new Promise((resolve, reject)=> {
-    setTimeout(() => {
-        //reject('errortiempo')
-        resolve([1,4,5])
-    }, 2000)
-})
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
 
-doWorkPromise.then(result => {
-    console.log(`Erfolg: ${result}`)
-}).catch(error => {
-    console.log(`Fehler: ${error}`)
+// add(1, 2).then(sum => {
+//     console.log(sum)
+//     add(sum, 5).then(sum => {
+//         console.log(sum)
+//     }).catch(e => {
+//         console.log(e)
+//     })
+// }).catch(e => {
+//     console.log(e)
+// })
+
+add (1, 1).then(sum => {
+    console.log(sum)
+    return add(sum, 4)
+}).then(sum2 => {
+    console.log(sum2)
 })
